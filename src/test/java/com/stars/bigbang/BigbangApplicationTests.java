5d55ed60524c838c465947fe55775103bc1fdb03
kassim <kassim_haddad@yahoo.fr>
2024-01-24 18:13:17 +0100

    Mon premier commit : l'initialisation

---
+++ src/test/java/com/stars/bigbang/BigbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stars.bigbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
