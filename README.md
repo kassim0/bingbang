f8d6616c263459ac97bc8cff0a595e6523c4b093
kassim <kassim_haddad@yahoo.fr>
2024-01-27 18:41:34 +0100

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Idée de nom du projet :
   - pegi island
   - bingbang
+
+key : d4df6345d7fb4a4e842849ef2bf16ba7
---
+++ backend/src/main/java/com/stars/bigbang/BigbangApplication.java
@@ -0,0 +1,44 @@
+package com.stars.bigbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/*import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;*/
+
+@SpringBootApplication
+public class BigbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbangApplication.class, args);
+
+		String url = "https://api.rawg.io/api/games?key=d4df6345d7fb4a4e842849ef2bf16ba7";
+		WebClient.Builder builder = WebClient.builder();
+
+		String allGames = builder.build().get().uri(url).retrieve().bodyToMono(String.class).block();
+
+		System.out.println("---------------------------------");
+		System.out.println(allGames);
+		System.out.println("---------------------------------");
+
+		// appel à l'API//
+/*		System.out.println("Début du protocole à la requête http: ");
+		URL url = new URL("https://api.rawg.io/api/games?key=d4df6345d7fb4a4e842849ef2bf16ba7");
+		System.out.println("URL crée");
+		try {
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			System.out.println("Connexion établi");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestMethod("GET");
+			System.out.println("Requête envoyé !");
+			System.out.println(con.getResponseCode());
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}*/
+	}
+
+}
