d4fd79de4c10d4efa8331d4f721e54cc2a48b401
kassim0 <kassim_haddad@yahoo.fr>
2025-08-03 15:57:58 +0200

    New Entity

--- backend/src/main/java/com/stars/bigbang/entity/Game.java
+++ backend/src/main/java/com/stars/bigbang/entity/Game.java
@@ -1,15 +1,23 @@
 package com.stars.bigbang.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name="game")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     String name;
     String pegi;
     String Plateform;
-    String[] categorie;
+    String categorie;
     LocalDateTime releaseDate;
     String note;
 }
