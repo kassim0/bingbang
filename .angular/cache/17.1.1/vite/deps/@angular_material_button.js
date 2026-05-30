e6dcd955e3bbb99af5e6afb3be30576bc9a03bff
kassim <kassim_haddad@yahoo.fr>
2026-05-30 22:13:36 +0200

    Suppression frontend

---
+++ .angular/cache/17.1.1/vite/deps/@angular_material_button.js
@@ -0,0 +1,36 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-CEIB3PZU.js";
+import "./chunk-2KRI54XE.js";
+import "./chunk-RGVVTHI2.js";
+import "./chunk-3UR46DIG.js";
+import "./chunk-WYHNGNSP.js";
+import "./chunk-52XDGCM2.js";
+import "./chunk-PP4FGZSX.js";
+import "./chunk-7VURRTRV.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
